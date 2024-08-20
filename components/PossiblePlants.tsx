46f35e3da88198beeef9a757f15a9219b0c336e2
Millie-Jay <millicentbowers@gmail.com>
2024-08-20 10:17:29 +0100

    repushing to resolve conflict

--- components/PossiblePlants.tsx
+++ components/PossiblePlants.tsx
@@ -73,7 +73,7 @@ const styles = StyleSheet.create({
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
-    shadowRadius: 5,
+    shadowRadius: 4,
     elevation: 3,
   },
   image: {
