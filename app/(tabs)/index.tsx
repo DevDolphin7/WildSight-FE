d2c7fb6dc30a3cb325ffbe723443667ff30ff43b
Dan Olver <devdolphin7@outlook.com>
2024-08-20 16:57:40 +0100

    Implement swipe functionality to home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,38 @@
-import { Image, StyleSheet, Button, ImageBackground } from "react-native";
+import { useRef } from "react";
+import { ImageBackground, View, Image, Button, StyleSheet } from "react-native";
+import PagerView from "react-native-pager-view";
 
-const backgroundImage = require("../../assets/images/icon.png");
+const backgroundImage = require("../../assets/images/home-screen-background.png");
 
 export default function HomeScreen() {
+  const ref = useRef<PagerView>(null);
   return (
     <ImageBackground
       source={backgroundImage}
       style={styles.background}
       resizeMode="cover"
     >
-      <Image
-        source={require("@/assets/images/partial-react-logo.png")}
-        style={styles.reactLogo}
-      />
-      <Button title="Get Started"></Button>
+      <PagerView style={styles.background} initialPage={0} ref={ref}>
+        <View style={styles.background} key="1">
+          <View style={styles.page}>
+            <Image
+              source={require("@/assets/images/partial-react-logo.png")}
+              style={styles.reactLogo}
+            />
+            <Button title="Get Started ->" onPress={() => {ref.current?.setPage(1)}}></Button>
+          </View>
+        </View>
+        <View style={styles.background} key="2">
+          <View style={styles.page}>
+            <Button title="<- Page 2 ->" onPress={() => {ref.current?.setPage(2)}}></Button>
+          </View>
+        </View>
+        <View style={styles.background} key="3">
+          <View style={styles.page}>
+            <Button title="<- Go Back ->" onPress={() => {ref.current?.setPage(0)}}></Button>
+          </View>
+        </View>
+      </PagerView>
     </ImageBackground>
   );
 }
@@ -24,10 +43,22 @@ const styles = StyleSheet.create({
     width: 290,
   },
   background: {
+    flex: 1,
+    width: 360,
+    height: 880,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  page: {
+    width: "90%",
+    height: "60%",
     display: "flex",
     justifyContent: "space-around",
-    height: "100%",
-    width: "100%",
     alignItems: "center",
+    borderWidth: 3,
+    borderRadius: 20,
+    borderColor: "#21514080",
+    backgroundColor: "#ffd5bd80",
   },
 });
