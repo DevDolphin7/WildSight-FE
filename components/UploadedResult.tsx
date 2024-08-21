3ef85832159cf262d9991afdb211ce715e0af274
Millie-Jay <millicentbowers@gmail.com>
2024-08-21 15:44:27 +0100

    Uploaded Results completed to the point of testing only

--- components/UploadedResult.tsx
+++ components/UploadedResult.tsx
@@ -1,5 +1,54 @@
-
+import axios from "axios";
 
 export const UploadedResult = () => {
-    
-}
+  //Plant Name back from AI
+  let scientificName = "Narcissus pseudonarcissus"; //must change this to marry up with the AI response.
+  let family = "";
+  let species = "";
+
+  function splitString(inputString: string): void {
+    const [f, ...rest] = inputString.split(" ");
+    family = f;
+    species = rest.join(" ");
+  }
+
+  splitString(scientificName);
+
+  //parametic endpoint of /taxa
+  let url = `https://api.inaturalist.org/v1/taxa?q=${family}%20${species}&per_page=1&order=desc&order_by=observations_count`;
+
+  return axios
+    .get<ApiResponse>(url)
+    .then((apiResponse) => {
+      const extractedData = extractData(apiResponse.data);
+      console.log(extractedData);
+      return extractedData;
+    })
+    .catch((error) => {
+      console.error("Error fetching data from API:", error);
+    });
+
+  interface Record {
+    preferred_common_name: string;
+    wikipedia_url: string;
+  }
+
+  interface Result {
+    record: Record;
+  }
+
+  interface ApiResponse {
+    results: Result[];
+  }
+
+  function extractData(data: ApiResponse) {
+    const result = data.results[0].record;
+    const preferredCommonName = result.preferred_common_name;
+    const wikipediaUrl = result.wikipedia_url;
+
+    return {
+      preferredCommonName,
+      wikipediaUrl,
+    };
+  }
+};
