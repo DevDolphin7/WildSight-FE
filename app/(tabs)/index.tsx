1ad57b10bb53093d6cd346568d9703897c5c7773
Dan Olver <devdolphin7@outlook.com>
2024-08-21 13:47:38 +0100

    Utilise StackNavigator to separate HomeScreen and SignUp components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,64 +1,19 @@
-import { useRef } from "react";
-import { ImageBackground, View, Image, Button, StyleSheet } from "react-native";
-import PagerView from "react-native-pager-view";
+import { StyleSheet } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "@/components/HomeScreen";
+import SignUp from "@/components/SignUp";
 
-const backgroundImage = require("../../assets/images/home-screen-background.png");
+const Stack = createStackNavigator();
 
-export default function HomeScreen() {
-  const ref = useRef<PagerView>(null);
+export default function Index() {
   return (
-    <ImageBackground
-      source={backgroundImage}
-      style={styles.background}
-      resizeMode="cover"
-    >
-      <PagerView style={styles.background} initialPage={0} ref={ref}>
-        <View style={styles.background} key="1">
-          <View style={styles.page}>
-            <Image
-              source={require("@/assets/images/partial-react-logo.png")}
-              style={styles.reactLogo}
-            />
-            <Button title="Get Started ->" onPress={() => {ref.current?.setPage(1)}}></Button>
-          </View>
-        </View>
-        <View style={styles.background} key="2">
-          <View style={styles.page}>
-            <Button title="<- Page 2 ->" onPress={() => {ref.current?.setPage(2)}}></Button>
-          </View>
-        </View>
-        <View style={styles.background} key="3">
-          <View style={styles.page}>
-            <Button title="<- Go Back ->" onPress={() => {ref.current?.setPage(0)}}></Button>
-          </View>
-        </View>
-      </PagerView>
-    </ImageBackground>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+    </Stack.Navigator>
   );
 }
 
 const styles = StyleSheet.create({
-  reactLogo: {
-    height: 178,
-    width: 290,
-  },
-  background: {
-    flex: 1,
-    width: 360,
-    height: 880,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  page: {
-    width: "90%",
-    height: "60%",
-    display: "flex",
-    justifyContent: "space-around",
-    alignItems: "center",
-    borderWidth: 3,
-    borderRadius: 20,
-    borderColor: "#21514080",
-    backgroundColor: "#ffd5bd80",
-  },
+  test: { backgroundColor: "red" },
 });
