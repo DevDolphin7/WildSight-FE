7ca76a3cab4ae2145bdc32b1fd284b3339700328
Jared-Ha <jared.harrison7@gmail.com>
2024-08-21 08:41:44 +0100

    add dynamic heatmap with markers when zoomed that brings in iNat API data from GET /Observations

---
+++ app/iNaturalist-api.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const inatApi = axios.create({
+	baseURL: "https://api.inaturalist.org/v1/",
+});
+
+export const getINatObservations = (
+	nelat: number,
+	nelng: number,
+	swlat: number,
+	swlng: number,
+	per_page: number
+) => {
+	const d1 = "2021-08-20";
+	const d2 = "2024-08-20";
+
+	return inatApi
+		.get("/observations", {
+			params: { d1, d2, nelat, nelng, swlat, swlng, per_page },
+		})
+		.then((response) => {
+			return response.data.results;
+		})
+		.catch((error) => {
+			console.log("Error getting iNat observations:", error);
+		});
+};
