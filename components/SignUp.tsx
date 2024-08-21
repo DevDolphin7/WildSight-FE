dffa3740095a52a9097d5dd6dc03f6900e65f1dd
Dan Olver <devdolphin7@outlook.com>
2024-08-21 13:47:59 +0100

    Add file

---
+++ components/SignUp.tsx
@@ -0,0 +1,9 @@
+import { Text, StyleSheet } from "react-native";
+
+export default function SignUp() {
+  return <Text style={styles.test}>Hello world!</Text>;
+}
+
+const styles = StyleSheet.create({
+  test: { backgroundColor: "red" },
+});
