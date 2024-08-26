74015cac4de7cf900b364061d92abb91fc658c52
Dan Olver <devdolphin7@outlook.com>
2024-08-26 18:43:21 +0100

    Add validateLogin function

--- scripts/__tests__/utils.test.js
+++ scripts/__tests__/utils.test.js
@@ -1,4 +1,4 @@
-import { validateSignUp } from "../utils";
+import { validateSignUp, validateLogin } from "../utils";
 
 describe("validateSignUp", () => {
   describe("Data types", () => {
@@ -99,6 +99,98 @@ describe("validateSignUp", () => {
       const inputCopy = JSON.parse(JSON.stringify(input));
 
       validateSignUp(input);
+
+      expect(input).toEqual(inputCopy);
+    });
+  });
+});
+describe("validateLogin", () => {
+  describe("Data types", () => {
+    test("Returns an object where every value is boolean", () => {
+      const input = {
+        usernameOrEmail: "",
+        password: "",
+      };
+      const output = validateLogin(input);
+
+      const values = Object.values(output);
+      expect(values.length).toBe(2);
+      values.forEach((value) => expect(typeof value).toBe("boolean"));
+    });
+    test("Returns {usernameOrEmail: false, password: false} on a blank object", () => {
+      const input = {};
+      const output = validateLogin(input);
+      expect(output).toEqual({
+        usernameOrEmail: false,
+        password: false,
+      });
+    });
+  });
+  describe("Functionality", () => {
+    test("Sets usernameOrEmail: false where characters for a username include anything other than [A-Za-z0-9-] or is less than 3 chars", () => {
+      const input = {
+        usernameOrEmail: "Hel_lo",
+        password: "He110@World",
+      };
+      const output = validateLogin(input);
+      expect(output).toEqual({
+        usernameOrEmail: false,
+        password: true,
+      });
+    });
+    test("Sets usernameOrEmail: false where an invalid email address is provided", () => {
+      const input = {
+        usernameOrEmail: "hello@world@com",
+        password: "He110@World",
+      };
+      const output = validateLogin(input);
+      expect(output).toEqual({
+        usernameOrEmail: false,
+        password: true,
+      });
+    });
+    test("Sets password: false where an invalid password is provided (needs 8-24 chars, an upper and lower case, a number and a symbol @#$%^&+=)", () => {
+      const input = {
+        usernameOrEmail: "Hello",
+        password: "HelloWorld",
+      };
+      const output = validateLogin(input);
+      expect(output).toEqual({
+        usernameOrEmail: true,
+        password: false,
+      });
+    });
+    test("Expects 2 keys: usernameOrEmail, password. Returns a value of false where any key is missing", () => {
+      const input = {
+        usernameOrEmail: "Hello",
+      };
+      const output = validateLogin(input);
+      expect(output).toEqual({
+        usernameOrEmail: true,
+        password: false,
+      });
+    });
+    test("Returns {usernameorEmail: true, password: true} on a valid input", () => {
+      const input = {
+        usernameOrEmail: "hello@world.com",
+        password: "He110@World",
+      };
+      const output = validateLogin(input);
+      expect(output).toEqual({
+        usernameOrEmail: true,
+        password: true,
+      });
+    });
+  });
+  describe("Function purity", () => {
+    test("Does not mutate the input", () => {
+      const input = {
+        usernameOrEmail: "Hello",
+        password: "He110@World",
+      };
+      const inputCopy = JSON.parse(JSON.stringify(input));
+
+      validateLogin(input);
 
       expect(input).toEqual(inputCopy);
     });
