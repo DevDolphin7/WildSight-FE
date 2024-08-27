255f0b0b318d01afa7be52be1d01c7afd50cd1b2
Jared-Ha <jared.harrison7@gmail.com>
2024-08-27 22:38:17 +0100

    populate maps with user sightings/ add toggle button + logic/ location alert

--- app/WildSight-api.ts
+++ app/WildSight-api.ts
@@ -1,41 +1,60 @@
 import axios from "axios";
 
 const wildSightApi = axios.create({
-	baseURL: "https://wildside-be.onrender.com/api",
+  baseURL: "https://wildside-be.onrender.com/api",
 });
 
 export const addUserSighting = (user_id: number, userSighting: any) => {
-	return wildSightApi
-		.post(`/sightings/user/${user_id}`, userSighting)
-		.then((response) => {
-			return response.data;
-		})
-		.catch((error) => {
-			console.log("Error:", error);
-		});
+  return wildSightApi
+    .post(`/sightings/user/${user_id}`, userSighting)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log("Error:", error);
+    });
 };
 
 export const getSightingById = (sighting_id: number) => {
-	return wildSightApi.get(`/sightings/${sighting_id}`).then((response) => {
-		return response.data.sighting;
-	});
+  return wildSightApi.get(`/sightings/${sighting_id}`).then((response) => {
+    return response.data.sighting;
+  });
 };
 
 export const getFavouritesById = (user_id: number) => {
-	return wildSightApi.get(`/mywildlife/users/${user_id}`).then((response) => {
-		return response.data.wildlife
-	})
-}
+  return wildSightApi.get(`/mywildlife/users/${user_id}`).then((response) => {
+    return response.data.wildlife;
+  });
+};
 
 export const deleteFavouritesById = (user_id: number, sighting_id: number) => {
-	return wildSightApi.delete(`/mywildlife/${sighting_id}/users/${user_id}`).then((response) => {
-
-		return response.data
-	})
-}
+  return wildSightApi
+    .delete(`/mywildlife/${sighting_id}/users/${user_id}`)
+    .then((response) => {
+      return response.data;
+    });
+};
 
 export const addFavouritesById = (user_id: number, sighting_id: number) => {
-	return wildSightApi.post(`/mywildlife/${sighting_id}/users/${user_id}`).then((response) => {
-		return response.data
-	})
-}
+  return wildSightApi
+    .post(`/mywildlife/${sighting_id}/users/${user_id}`)
+    .then((response) => {
+      return response.data;
+    });
+};
+
+export const getSightingsByUserId = (
+  user_id: Number,
+  nelat: number,
+  nelng: number,
+  swlat: number,
+  swlng: number
+) => {
+  return wildSightApi
+    .get(
+      `/sightings/user/${user_id}?swlat=${swlat}&swlong=${swlng}&nelat=${nelat}&nelong=${nelng}`
+    )
+    .then((response) => {
+      return response.data.sightings;
+    });
+};
--- components/SingleWildlife.tsx
+++ components/SingleWildlife.tsx
@@ -5,146 +5,167 @@ import { useRoute } from "@react-navigation/native";
 import { getSightingById } from "@/app/WildSight-api";
 
 export default function SingleWildlife() {
-	const [singleWildlife, setSingleWildlife] = useState({});
-	const route = useRoute();
-	const { iNatId, WildSightSightingId } = route.params;
-	// console.log(WildSightSightingId);
-	// we will need to also bring in the sighting_id from any clicked WildSight sightings on the map or in users personal list
-	if (iNatId) {
-		useEffect(() => {
-			getINatObservationById(iNatId)
-				.then((observation) => {
-					setSingleWildlife(observation);
-				})
-				.catch((error) => console.log(error));
-		}, [iNatId]);
-	}
-	if (WildSightSightingId) {
-		useEffect(() => {
-			getSightingById(WildSightSightingId)
-				.then((sighting) => {
-					console.log("SWL hello", sighting);
-					setSingleWildlife(sighting);
-				})
-				.then(() => {
-					console.log("SWL", singleWildlife);
-				});
-		}, [WildSightSightingId]);
-	}
+  const [singleWildlife, setSingleWildlife] = useState({});
+  const [loading, setLoading] = useState(true);
+  const route = useRoute();
+  const { iNatId, WildSightSightingId } = route.params;
 
-	return (
-		<ScrollView>
-			{!WildSightSightingId && !iNatId ? (
-				<View>No sighting ID found</View>
-			) : (
-				<View style={styles.container}>
-					{WildSightSightingId ? (
-						<View>
-							<Text style={styles.speciesGuess}>
-								{singleWildlife?.common_name}
-							</Text>
-							<Text style={styles.observerInfo}>User ID: {}</Text>
-							<Image
-								source={{
-									uri: singleWildlife?.uploaded_image,
-								}}
-								style={styles.fullWidthImage}
-							/>
-							<Text style={styles.summaryTitle}>Wikipedia Summary:</Text>
-							<Text style={styles.summaryText}>
-								{singleWildlife?.wikipedia_url
-									? singleWildlife?.wikipedia_url.replace(/<\/?[^>]+(>|$)/g, "")
-									: null}
-							</Text>
-						</View>
-					) : null}
-					{iNatId ? (
-						<View>
-							<Text style={styles.speciesGuess}>
-								{singleWildlife.species_guess}
-							</Text>
-							<Text style={styles.observerInfo}>
-								{singleWildlife.user?.name
-									? `Observer: ${singleWildlife.user.name}`
-									: null}
-							</Text>
-							<Text style={styles.observationsCount}>
-								Observations: {singleWildlife.user?.observations_count}
-							</Text>
-							{singleWildlife.taxon?.default_photo?.medium_url && (
-								<Image
-									source={{
-										uri: singleWildlife.taxon.default_photo.medium_url,
-									}}
-									style={styles.fullWidthImage}
-								/>
-							)}
-							<Text style={styles.summaryTitle}>Wikipedia Summary:</Text>
-							<Text style={styles.summaryText}>
-								{singleWildlife.taxon?.wikipedia_summary.replace(
-									/<\/?[^>]+(>|$)/g,
-									""
-								)}
-							</Text>
-							<View style={styles.observationPhotosContainer}>
-								<Text style={styles.photosTitle}>User Photos:</Text>
-								{singleWildlife.observation_photos?.map((photo, index) => (
-									<Image
-										key={index}
-										source={{ uri: photo.photo.url }}
-										style={styles.smallPhoto}
-									/>
-								))}
-							</View>
-						</View>
-					) : null}
+  useEffect(() => {
+    if (iNatId) {
+      getINatObservationById(iNatId)
+        .then((observation) => {
+          setSingleWildlife(observation);
+          setLoading(false);
+        })
+        .catch((error) => console.log(error));
+    } else if (WildSightSightingId) {
+      getSightingById(WildSightSightingId)
+        .then((sighting) => {
+          setSingleWildlife(sighting);
+          setLoading(false);
+        })
+        .catch((error) => console.log(error));
+    } else {
+      setLoading(false);
+    }
+  }, [iNatId, WildSightSightingId]);
 
-				</View>
-			)}
-		</ScrollView>
-	);
+  return (
+    <View style={styles.container}>
+      {loading ? (
+        <View style={styles.loadingContainer}>
+          <Text style={styles.loadingText}>Loading...</Text>
+        </View>
+      ) : (
+        <ScrollView>
+          {!WildSightSightingId && !iNatId ? (
+            <View>
+              <Text>No sighting ID found</Text>
+            </View>
+          ) : (
+            <View>
+              {WildSightSightingId ? (
+                <View>
+                  <Text style={styles.speciesGuess}>
+                    {singleWildlife?.common_name}
+                  </Text>
+                  <Text style={styles.observerInfo}>User ID: {}</Text>
+                  <Image
+                    source={{
+                      uri: singleWildlife?.uploaded_image,
+                    }}
+                    style={styles.fullWidthImage}
+                  />
+                  <Text style={styles.summaryTitle}>Wikipedia Summary:</Text>
+                  <Text style={styles.summaryText}>
+                    {singleWildlife?.wikipedia_url
+                      ? singleWildlife?.wikipedia_url.replace(
+                          /<\/?[^>]+(>|$)/g,
+                          ""
+                        )
+                      : null}
+                  </Text>
+                </View>
+              ) : null}
+              {iNatId ? (
+                <View>
+                  <Text style={styles.speciesGuess}>
+                    {singleWildlife.species_guess}
+                  </Text>
+                  <Text style={styles.observerInfo}>
+                    {singleWildlife.user?.name
+                      ? `Observer: ${singleWildlife.user.name}`
+                      : null}
+                  </Text>
+                  <Text style={styles.observationsCount}>
+                    Observations: {singleWildlife.user?.observations_count}
+                  </Text>
+                  {singleWildlife.taxon?.default_photo?.medium_url && (
+                    <Image
+                      source={{
+                        uri: singleWildlife.taxon.default_photo.medium_url,
+                      }}
+                      style={styles.fullWidthImage}
+                    />
+                  )}
+                  <Text style={styles.summaryTitle}>Wikipedia Summary:</Text>
+                  <Text style={styles.summaryText}>
+                    {singleWildlife.taxon?.wikipedia_summary.replace(
+                      /<\/?[^>]+(>|$)/g,
+                      ""
+                    )}
+                  </Text>
+                  <View style={styles.observationPhotosContainer}>
+                    <Text style={styles.photosTitle}>User Photos:</Text>
+                    {singleWildlife.observation_photos?.map((photo, index) => (
+                      <Image
+                        key={index}
+                        source={{ uri: photo.photo.url }}
+                        style={styles.smallPhoto}
+                      />
+                    ))}
+                  </View>
+                </View>
+              ) : null}
+            </View>
+          )}
+        </ScrollView>
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		padding: 15,
-	},
-	speciesGuess: {
-		fontSize: 28,
-		fontWeight: "bold",
-	},
-	fullWidthImage: {
-		height: 250,
-		marginBottom: 20,
-	},
-	observerInfo: {
-		marginTop: 10,
-	},
-	observationsCount: {
-		marginBottom: 10,
-	},
-	summaryTitle: {
-		fontSize: 22,
-		fontWeight: "bold",
-		marginTop: 10,
-	},
-	summaryText: {
-		fontSize: 16,
-		marginTop: 10,
-	},
-	observationPhotosContainer: {
-		flexDirection: "row",
-		flexWrap: "wrap",
-		marginTop: 20,
-	},
-	photosTitle: {
-		fontSize: 20,
-		fontWeight: "bold",
-		marginBottom: 10,
-	},
-	smallPhoto: {
-		width: 60,
-		height: 60,
-		margin: 5,
-	},
+  container: {
+    flex: 1,
+    padding: 15,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loadingText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "gray",
+  },
+  speciesGuess: {
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+  fullWidthImage: {
+    height: 250,
+    marginBottom: 20,
+  },
+  observerInfo: {
+    marginTop: 10,
+  },
+  observationsCount: {
+    marginBottom: 10,
+  },
+  summaryTitle: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  summaryText: {
+    fontSize: 16,
+    marginTop: 10,
+  },
+  observationPhotosContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginTop: 20,
+  },
+  photosTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  smallPhoto: {
+    width: 60,
+    height: 60,
+    margin: 5,
+  },
 });
