7a485dcfda759d899ba28cac772a0940fcd19b26
Jared-Ha <jared.harrison7@gmail.com>
2024-08-27 22:44:09 +0100

    add pssible camera jounrey stlye updates

--- components/PlantIdentification.tsx
+++ components/PlantIdentification.tsx
@@ -1,11 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { View, Image, StyleSheet, Button, Text, Alert } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Image,
+  StyleSheet,
+  Button,
+  Text,
+  Alert,
+  ActivityIndicator,
+} from "react-native";
 import { RouteProp } from "@react-navigation/native";
 import axios from "axios";
 import { RootStackParamList } from "../types";
 import { Picker } from "@react-native-picker/picker";
-
-
 
 const FormData = require("form-data");
 
@@ -27,8 +33,6 @@ const PlantIdentification: React.FC<PlantIdentificationProps> = ({
   const [organType, setOrganType] = useState("leaf");
   const [uploading, setUploading] = useState(false);
 
-
-
   const handleSubmit = () => {
     setUploading(true);
 
@@ -48,27 +52,25 @@ const PlantIdentification: React.FC<PlantIdentificationProps> = ({
     const apiKey = "2b107NOh3HG5cNE31rCZUUj0u";
     const url = `https://my-api.plantnet.org/v2/identify/${project}?api-key=${apiKey}&include-related-images=${includeRelatedImages}&no-reject=${noReject}&nb-results=${nbResults}&lang=${lang}`;
 
-
-    
     axios
-      .post(
-        url,
-        formData,
-        {
-          headers: {'Content-Type': 'multipart/form-data'}
-        }
-      )
+      .post(url, formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
       .then((response) => {
         const result = response.data;
-        navigation.navigate('AiResult', {data: result, latPosition, longPosition, photoUri})
+        navigation.navigate("AiResult", {
+          data: result,
+          latPosition,
+          longPosition,
+          photoUri,
+        });
 
         Alert.alert(
           "We've found some matches",
-          `Choose the wildlife you've sightedkmn`
+          `Choose the wildlife you've sighted`
         );
       })
       .catch((error) => {
-
         console.error("Error submitting plant identification:", error);
         Alert.alert(
           "Error",
@@ -80,30 +82,40 @@ const PlantIdentification: React.FC<PlantIdentificationProps> = ({
       });
   };
 
-//   if (setUploading) {
-//     return <p> Getting Plant Info</p>
-//   }
-
   return (
     <View style={styles.container}>
+      {uploading && (
+        <View style={styles.loadingOverlay}>
+          <ActivityIndicator size="large" color="#ffffff" />
+          <Text style={styles.loadingText}>Loading...</Text>
+        </View>
+      )}
       <Image source={{ uri: photoUri }} style={styles.preview} />
       <Text style={styles.label}>Select Organ Type:</Text>
-      <Picker
-        selectedValue={organType}
-        style={styles.picker}
-        onValueChange={(itemValue) => setOrganType(itemValue)}
-      >
-        <Picker.Item label="Leaf" value="leaf" />
-        <Picker.Item label="Flower" value="flower" />
-        <Picker.Item label="Fruit" value="fruit" />
-        <Picker.Item label="Bark" value="bark" />
-        <Picker.Item label="other" value="auto" />
-      </Picker>
-      <Button title="Submit" onPress={handleSubmit} disabled={uploading} />
-      <Button
-        title="Retake Photo"
-        onPress={() => navigation.navigate("Camera")}
-      />
+      <View style={styles.pickerContainer}>
+        <Picker
+          selectedValue={organType}
+          style={styles.picker}
+          onValueChange={(itemValue) => setOrganType(itemValue)}
+        >
+          <Picker.Item label="Leaf" value="leaf" />
+          <Picker.Item label="Flower" value="flower" />
+          <Picker.Item label="Fruit" value="fruit" />
+          <Picker.Item label="Bark" value="bark" />
+          <Picker.Item label="Other" value="auto" />
+        </Picker>
+      </View>
+      <View style={styles.buttonContainer}>
+        <View style={styles.buttonWrapper}>
+          <Button title="Submit" onPress={handleSubmit} disabled={uploading} />
+        </View>
+        <View style={styles.buttonWrapper}>
+          <Button
+            title="Retake Photo"
+            onPress={() => navigation.navigate("Camera")}
+          />
+        </View>
+      </View>
     </View>
   );
 };
@@ -121,12 +133,42 @@ const styles = StyleSheet.create({
   },
   label: {
     fontSize: 16,
-    marginBottom: 10,
+    marginBottom: 5,
+  },
+  pickerContainer: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    marginBottom: 20,
+    width: "80%",
   },
   picker: {
-    height: 150,
-    width: 150,
-    marginBottom: 20,
+    height: 50,
+    width: "100%",
+  },
+  buttonContainer: {
+    width: "80%",
+    marginVertical: 20,
+  },
+  buttonWrapper: {
+    marginVertical: 10,
+  },
+  loadingOverlay: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 1,
+  },
+  loadingText: {
+    marginTop: 10,
+    color: "#ffffff",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
 
