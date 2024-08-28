66ebbc6e94fe97ad57e2ad8725e54997b7bd8c23
Dan Olver <devdolphin7@outlook.com>
2024-08-28 17:33:02 +0100

    Update navbar icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,16 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import { Image } from "react-native";
 
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+
+const HomeIcon = require("../../assets/images/HomeIcon.png");
+const MyWildlifeIcon = require("../../assets/images/MyWildlifeIcon.png");
+const MapIcon = require("../../assets/images/MapIcon.png");
+const AddSightingIcon = require("../../assets/images/AddSightingIcon.png");
+
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -19,11 +26,8 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "Home",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "home" : "home-outline"}
-              color={color}
-            />
+          tabBarIcon: () => (
+            <Image source={HomeIcon} style={{ width: 28, height: 28 }} />
           ),
         }}
       />
@@ -31,11 +35,26 @@ export default function TabLayout() {
         name="explore"
         options={{
           title: "My Wildlife",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "code-slash" : "code-slash-outline"}
-              color={color}
-            />
+          tabBarIcon: () => (
+            <Image source={MyWildlifeIcon} style={{ width: 28, height: 28 }} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="map"
+        options={{
+          title: "Map",
+          tabBarIcon: () => (
+            <Image source={MapIcon} style={{ width: 21, height: 28 }} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="camera"
+        options={{
+          title: "Add Sighting",
+          tabBarIcon: () => (
+            <Image source={AddSightingIcon} style={{ width: 28, height: 22 }} />
           ),
         }}
       />
